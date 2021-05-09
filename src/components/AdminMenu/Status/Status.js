fc0fbbef92f85b5677023a3effbf5fafe5ac014a
Kevin Ung <ung.kevin78@gmail.com>
2021-05-09 11:30:41 -0600

    Initialize Status Page

---
+++ src/components/AdminMenu/Status/Status.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Status () {
+    return (
+        <div className="status-page">
+            <h2>Status</h2>
+        </div>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import AdminMenu from './components/AdminMenu/AdminMenu';
 import MenuItem from './components/MenuItem';
+import Status from './components/AdminMenu/Status/Status';
 
 // const path = '/craft-menu/';
 const path = '/';
@@ -10,10 +11,10 @@ const path = '/';
 export default function route(routeProps) {
     return (
         <Switch>
-            <Route path={`${path}admin/status`} component="" />
-            <Route path={`${path}admin`} render={(props) => <AdminMenu {...props} {...routeProps}/>} />
-            <Route path={`${path}:id`} render={(props) => <MenuItem {...props} {...routeProps}/>} />
-            <Route exact path={`${path}`} render={(props) => <Menu {...props} {...routeProps}/>} />
+            <Route path={`${path}admin/status`} render={(props) => <Status {...props} {...routeProps} />} />
+            <Route path={`${path}admin`} render={(props) => <AdminMenu {...props} {...routeProps} />} />
+            <Route path={`${path}:id`} render={(props) => <MenuItem {...props} {...routeProps} />} />
+            <Route exact path={`${path}`} render={(props) => <Menu {...props} {...routeProps} />} />
         </Switch>
     )
 }
